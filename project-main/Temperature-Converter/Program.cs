584e98994b90ece491da2608f006dda54275303a
mchilders14 <childersmatt4@gmail.com>
2021-07-02 01:17:56 -0400

    adding temperature converter

--- project-main/Temperature-Converter/Program.cs
+++ project-main/Temperature-Converter/Program.cs
@@ -6,7 +6,31 @@ namespace Temperature_Converter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the temperature converter");
+
+            string choice = "y";
+
+            while (choice.Equals("y")) 
+            {
+                string number = "";
+                double tempF;
+
+                while (!Double.TryParse(number, out tempF))
+                {
+                    Console.Write("\nEnter degrees in Fahrenheit: ");
+                    number = Console.ReadLine();
+                }
+
+                double tempC;
+                tempC = (tempF - 32) * (5.0 / 9.0);
+
+                Console.WriteLine("Degrees in Celsius: " + tempC);
+
+                Console.Write("\nContinue(y/n)?");
+                choice = Console.ReadLine();
+            }
+
+            Console.WriteLine("Exiting application");
         }
     }
 }
