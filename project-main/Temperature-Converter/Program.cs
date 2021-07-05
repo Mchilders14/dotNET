cd529f9abdb955879878322b8bce07f0a37b3519
mchilders14 <childersmatt4@gmail.com>
2021-07-05 16:05:09 -0400

    More formatting for temp converter

--- project-main/Temperature-Converter/Program.cs
+++ project-main/Temperature-Converter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Temperature_Converter
 {
@@ -6,6 +7,9 @@ namespace Temperature_Converter
     {
         static void Main(string[] args)
         {
+            NumberFormatInfo setPrecision = new NumberFormatInfo();
+            setPrecision.NumberDecimalDigits = 3;   // Setting number of decimal digits
+
             Console.WriteLine("Welcome to the temperature converter");
 
             string choice = "y";
@@ -15,7 +19,7 @@ namespace Temperature_Converter
                 string number = "";
                 double tempF;
 
-                while (!Double.TryParse(number, out tempF))
+                while (!Double.TryParse(number, out tempF)) // function to parse string -> double : returns boolean
                 {
                     Console.Write("\nEnter degrees in Fahrenheit: ");
                     number = Console.ReadLine();
@@ -24,7 +28,7 @@ namespace Temperature_Converter
                 double tempC;
                 tempC = (tempF - 32) * (5.0 / 9.0);
 
-                Console.WriteLine($"Degrees in Celsius: {tempC}");
+                Console.WriteLine($"Degrees in Celsius: {tempC.ToString("string", setPrecision)}"); // Using set precision instead of rounding
 
                 Console.Write("\nContinue(y/n)?");
                 choice = Console.ReadLine();
