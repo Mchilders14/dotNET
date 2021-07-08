5c69b7a39f24153d1bb8f5e7af406771bb46aa87
mchilders14 <childersmatt4@gmail.com>
2021-07-08 10:57:42 -0400

    adding entity framework

---
+++ example-main/EntityFramework-Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFramework_Example   // Installed EntityFramework.SqlServer && EntityFramework.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
