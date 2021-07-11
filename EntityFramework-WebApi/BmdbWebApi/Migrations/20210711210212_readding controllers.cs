7fd487dc4af8ff1978f487c3a10761ec2f2c899c
mchilders14 <childersmatt4@gmail.com>
2021-07-11 17:08:39 -0400

    adding changes

---
+++ EntityFramework-WebApi/BmdbWebApi/Migrations/20210711210212_readding controllers.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BmdbWebApi.Migrations
+{
+    public partial class readdingcontrollers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieCollections",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PurchasePrice = table.Column<decimal>(type: "decimal(11,2)", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieCollections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MovieCollections_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieCollections_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieCollections_MovieId",
+                table: "MovieCollections",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieCollections_UserID",
+                table: "MovieCollections",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieCollections");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- EntityFramework-WebApi/BmdbWebApi/Models/MovieCollection.cs
+++ EntityFramework-WebApi/BmdbWebApi/Models/MovieCollection.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BmdbWebApi.Models
 {
     public class MovieCollection
     {
         public int Id { get; set; }
+
+        [Column(TypeName = "decimal(11,2)")]
         public decimal PurchasePrice { get; set; }
 
         public int MovieId { get; set; }
